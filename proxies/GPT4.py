ea8e9fe751ce28795bcb2209390dea2cac495bf4
Francesco Nacinovich <francesco.nacinovich@gmail.com>
2024-03-01 18:41:49 +0000

    switched to html output

--- proxies/GPT4.py
+++ proxies/GPT4.py
@@ -14,12 +14,11 @@ def read_text_GPT4(base64_image):
             "role": "user",
             "content": [
                 {"type": "text", "text": "Your goal is to read ALL the text in the provided image."},
-                {"type": "text", "text": "Return JSON document with data. Only return JSON not other text. Avoid newlines and any text that is not pure JSON. If the document contains sections and subsections please clearly describe them. Do not try to read text inside images. Keep the original language. Try to structure the response using the following hierarchy: Main Title > Chapter > Paragraph. Use the following JSON as a template: {\"documentTitle\":\"The Title of the Document\",\"chapters\":[{\"chapterTitle\":\"Chapter 1\",\"sections\":[{\"sectionTitle\":\"Section 1.1\",\"sectionContent\":\"Content of Section 1.1.\"},{\"sectionTitle\":\"Section 1.2\",\"sectionContent\":\"Content of Section 1.2.\"}]},{\"chapterTitle\":\"Chapter 2\",\"sections\":[{\"sectionTitle\":\"Section 2.1\",\"sectionContent\":\"Content of Section 2.1.\"},{\"sectionTitle\":\"Section 2.2\",\"sectionContent\":\"Content of Section 2.2.\"},{\"sectionTitle\":\"Section 2.3\",\"sectionContent\":\"Content of Section 2.3.\"}]}]}"},
+                {"type": "text", "text": "Return an HTML table with data. Only return HTML not other text. Do not try to read text inside images. Keep the original language. Try to break the text into semantically meaningful sections. Use the following HTML structure for the output: <table><tr><td>Section Title</td><td>Section Text</td></tr><tr><td>Section Title</td><td>Section Text</td></tr></table>"},
                 {"type": "text", "text": "If the image contains partial pages ignore them and all the text in them."},
                 {"type": "text", "text": "If the text contains foreign letters like accents (examples: à, è, ì, ć, č, ž) please keep them as is and do not convert them to Unicode codes like '\u00f3'."},                
                 {"type": "text", "text": "Break the text into different sections only if there is a very clear reason for it like a new line and an indentation on the new line. In case of doubt, do not create a new section and keep the text in the same section."},
                 {"type": "text", "text": "Try your best to read all the text in the image. Do not leave anything out."},
-                {"type": "text", "text": "Before sending the response validate the JSON and fix it in case it is not a valid JSON."},
                 {
                     "type": "image_url",
                     "image_url": {
